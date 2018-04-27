4c56bf917ee3be73a3029880484bed5c0e521194
py-john <pyjohn3@gmail.com>
2018-04-27 16:08:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## replcopy
+
+Simple script to parse clipboard copied code from the interpreter REPL. 
+
+The idea is to be able to test code in the interpreter, copy it, and then use `replcopy.copy()` to bring a parsed version to the clipboard that can be pasted into a code editor and run as a normal script. 
+
+The parsed version of the code has as much unnecessary and potentially error causing text removed as possible, such as `>>>`, `...`, tracebacks and other output.
---
+++ replcopy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import pyperclip
+
+def parse_code(s):
+    result = []
+    lines = s.splitlines()
+    end = len(lines)
+    for index, line in enumerate(lines):
+        next_index = index + 1
+        if line.startswith('>>>') and next_index != end:
+            block = [line[4:]]
+            while next_index != end and lines[next_index].startswith('...'):
+                block.append(lines[next_index][4:])
+                next_index += 1
+                if next_index == end:
+                    print('first while index end')
+            while next_index != end and not any(lines[next_index].startswith(s) for s in ('>>>', 'Trace', '  File')):
+                next_index +=1
+                if next_index == end:
+                    print('2nd while index end')
+
+            if next_index != end and lines[next_index].startswith('>>>'):
+                result.append('\n'.join(block))
+
+    if lines[-1].startswith('>>>'):
+        result.append(lines[-1][4:])
+    return '\n'.join(result)
+
+def copy(to_end=False):
+    if to_end:
+        with open('/Users/john/Terminal Saved Output', 'r') as f:
+            output = f.read().replace('bpython', 'Python')
+            code = output.split('\nPython')[-1]
+    else:
+        code = pyperclip.paste()
+        # print(code)
+        # return code
+    pyperclip.copy(parse_code(code))
+    return None
+
